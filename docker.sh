8f3f22c7be9b9d2784883736d1df346fb7a9c7c4
schong94 <schong.code@gmail.com>
2022-02-20 12:05:06 +0000

    Initialized centos branch

---
+++ docker.sh
@@ -0,0 +1,19 @@
+# Install docker
+sudo yum remove docker \
+		docker-client \
+		docker-client-latest \
+		docker-common \
+		docker-latest \
+		docker-latest-logrotate \
+		docker-logrotate \
+		docker-engine
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum -y install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
